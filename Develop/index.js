790a929aad6bfba0a53e55c4ca603e796c61eed6
Michael Kendrick <michael@locumsmart.net>
2022-04-06 09:34:22 -0500

    post tutor

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What's the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "ipnut",
+    name: "contents",
+    message: "What would you like to include in the table of contents?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    writeToFile("testReadme.md", generateMarkdown({ ...answers }));
+    // Use user feedback for... whatever!!
+  });
+}
 
 // Function call to initialize app
 init();
